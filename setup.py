1c09c3d08e0464ad62c9235cf3850db352129639
Daniel <dbiesecke@gmail.com>
2026-05-26 03:23:36 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,46 @@
-#!/usr/bin/env python
-"""
-setup.py PuPI setup project file
-"""
-__author__ = "Michael Montuori [michael.montuori@gmail.com]"
-__copyright__ = "Copyright 2017 Michael Montuori. All rights reserved."
-__credits__ = []
-__license__ = "GPLv3"
-__version__ = "1.0"
-__revision__ = "0"
+#!/usr/bin/env python3
+"""Legacy setuptools entrypoint.
 
-import os
+The same metadata is also declared in pyproject.toml for modern PEP 517
+builds. Keep this file dependency-free so packaging does not import
+sprinkle.py before install requirements exist.
+"""
+
+from pathlib import Path
+
 from setuptools import setup
-from setuptools import find_packages
-import sprinkle
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
 
-this = os.path.dirname(os.path.realpath(__file__))
+ROOT = Path(__file__).resolve().parent
+RUNTIME_REQUIREMENTS = [
+    "daemons>=1.3.0",
+    "filelock>=3.0.10",
+    "progress>=1.4",
+]
 
-def read(name):
-    with open(os.path.join(this, name)) as f:
-        return f.read()
 
 setup(
-    name='sprinkle-py',
-    version=sprinkle.__version__+'.'+sprinkle.__revision__,
-    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    packages=['libsprinkle'],
-    install_requires=read('requirements.txt'),
-    url='https://gitlab.com/mmontuori/sprinkle',
-    license='GPLv3',
+    name="sprinkle-py",
+    version="1.0.0",
+    packages=["libsprinkle"],
+    py_modules=["sprinkle"],
+    scripts=["sprinkle.py"],
+    install_requires=RUNTIME_REQUIREMENTS,
+    url="https://gitlab.com/mmontuori/sprinkle",
+    license="GPLv3",
     include_package_data=True,
-    author='mmontuori',
-    author_email='michael.montuori@gmail.com',
-    description='Sprinkle is a volume clustering utility based on [RClone](https://rclone.org).',
-    long_description=long_description,
-    keywords='sprinkle cloud backup restore',
-    zip_safe=True,
+    author="Michael Montuori",
+    author_email="michael.montuori@gmail.com",
+    description="Sprinkle is a volume clustering utility based on RClone.",
+    long_description=(ROOT / "README.md").read_text(),
     long_description_content_type="text/markdown",
+    keywords="sprinkle cloud backup restore rclone",
+    zip_safe=True,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
-    scripts=['sprinkle.py']
 )
